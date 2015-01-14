d61f92e96c3dac7f6ee6c7f631e021f8b7b0ff60
Renato Ramiro <renato.ipj@gmail.com>
2015-01-14 10:34:55 -0300

    Limpar form depois do submit e adicionado botão reset no form

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 	var app = angular.module('store', []);
 
 	app.controller('StoreController', function(){
-		// var store = this;
+		var store = this;
 		var gems = [
 			{name: 'Product 1', price: 10.10},
 			{name: 'Product 2', price: 10.10},
@@ -12,23 +12,33 @@
 
 		this.products = gems;
 	});
-
 	app.controller('ProductsController', ['$scope', function($scope){
-			this.product = {};
+				this.product = {};
+		
+				this.addProduct = function (store) {
+					this.product = {name: this.name, price: this.price};
+					//Adicionar produto no array de produtos
+					store.products.push(this.product);
+					//Limpar o formulário
+					this.name = '';
+					this.price = '';
+					$scope.productForm.$setPristine();
+				};
 	
-			this.addProduct = function (store) {
-				this.product = {name: this.name, price: this.price};
-				store.products.push(this.product);
-			};
+				this.removeProduct = function($event, store, product){
+					$event.preventDefault();
+					$.each(store.products, function(i){
+				    if(store.products[i].$$hashKey === product.$$hashKey) {
+			        store.products.splice(i,1);
+			        return false;
+				    }
+					});
+				};
 
-			this.removeProduct = function($event, store, product){
-				$event.preventDefault();
-				$.each(store.products, function(i){
-			    if(store.products[i].$$hashKey === product.$$hashKey) {
-		        store.products.splice(i,1);
-		        return false;
-			    }
-				});
-			};
-		}]);
+				this.reset = function () {
+					this.name = '';
+					this.price = '';
+					$scope.productForm.$setPristine();
+				}
+			}]);
 })();
